f5d3912a99ed8a6a5086ec3f99328dd1d1fc8407
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-24 09:37:04 -0600

    Import Home component to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
 
-import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import Home from '../Home/Home';
 
 const App = () => {
 
   return (
     <main className='app'>
+      <h1>BARD BUDDY</h1>
       <nav>
         <NavLink to='/'>HOME</NavLink>
         <NavLink to='/'>SAVED PASSAGES</NavLink>
       </nav>
       <Switch>
-        <Route exact path='/' />
+        <Route exact path='/' component={Home}/>
         <Route path={['/tragedies', '/comedies', '/histories', 'sonnets']} />
         <Route path={['/tragedies/:play', 'comedies/:play', '/histoires/:play']} />
         <Route path='#' />
@@ -21,4 +23,4 @@ const App = () => {
 
 }
 
-export default App;
+export default App
