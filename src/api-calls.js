002d4d2955dd3a2568b6e9b455c92cb2625eb4e9
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-24 09:55:11 -0600

    Refactor api calls into one function

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,21 +1,7 @@
 
 const baseURL = `https://bard-buddy-api.herokuapp.com`
 
-export const getPlay = (play) => {
-  fetch(`${baseURL}/play/${play}`)
+export const getPlayData = (data, play) => {
+  fetch(`${baseURL}/${data}/${play}`)
   .then(res => res.json())
-}
-
-export const getPlayCharacters = (play) => {
-  fetch(`${baseURL}/characters/${play}`)
-  .then(res => res.json())
-}
-
-export const getPlayChapters = (play) => {
-  fetch(`${baseURL}/chapters/${play}`)
-  .then(res => res.json())
-}
-
-export const getPlaysByGenre = (genre) => {
-  fetch(`${baseURL}/allTitles/${genre}`)
 }
