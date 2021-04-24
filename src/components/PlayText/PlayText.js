5fc64ef1dcb873769c0b0b1202067d2f8ba60a05
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-24 14:58:11 -0600

    Improve character name appearance on play text

--- src/components/PlayText/PlayText.js
+++ src/components/PlayText/PlayText.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getScene } from '../../api-calls'
+import { getScene, getPlayData } from '../../api-calls'
 import { numToRoms } from '../../helper-functions'
 
 export default class PlayText extends Component {
@@ -10,20 +10,24 @@ export default class PlayText extends Component {
       play: play,
       act: act,
       scene: scene,
-      text: null
+      text: null,
+      characters: []
     }
   }
 
   componentDidMount() {
     getScene(this.state.play, this.state.act, this.state.scene)
     .then(data => this.setState({ text: data }))
+
+    getPlayData('characters', this.state.play)
+    .then(data => this.setState({ characters: data }))
   }
 
   grafCompiler() {
     return this.state.text.map(graf => {
       return (
         <div>
-          {graf.charid === 'xxx' ? <p></p> : <p><strong>{graf.charid.toUpperCase()}</strong></p>}
+          {graf.charid === 'xxx' ? <p></p> : <p><strong>{this.state.characters.find(character => character.charid === graf.charid).charname.toUpperCase()}</strong></p>}
           {graf.plaintext.split(`n[p]`).map(line => {
             return <p>{line}</p>
           })}
