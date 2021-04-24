33935174aec2848b3c8da03cdeff33c45ecb5b73
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-24 09:03:59 -0600

    Add network requests to api calls file

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,5 +1,21 @@
 
-export const getPlay = () => {
-  return fetch('http://localhost:3005/allswell')
-  .then(response => response.json())
+const baseURL = `https://bard-buddy-api.herokuapp.com`
+
+export const getPlay = (play) => {
+  fetch(`${baseURL}/play/${play}`)
+  .then(res => res.json())
+}
+
+export const getPlayCharacters = (play) => {
+  fetch(`${baseURL}/characters/${play}`)
+  .then(res => res.json())
+}
+
+export const getPlayChapters = (play) => {
+  fetch(`${baseURL}/chapters/${play}`)
+  .then(res => res.json())
+}
+
+export const getPlaysByGenre = (genre) => {
+  fetch(`${baseURL}/allTitles/${genre}`)
 }
