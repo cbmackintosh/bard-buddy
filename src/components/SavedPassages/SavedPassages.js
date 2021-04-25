dbdd37e10a9b6c7447a6e51cc6fef96d7c2793d4
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-25 16:10:42 -0600

    Display saved lines on saved passages page

--- src/components/SavedPassages/SavedPassages.js
+++ src/components/SavedPassages/SavedPassages.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PlayLine from '../PlayLine/PlayLine'
 
 export default class SavedPassages extends Component {
   constructor() {
@@ -9,15 +10,37 @@ export default class SavedPassages extends Component {
   }
 
   componentDidMount() {
-    this.setState({ savedPassages: localStorage.getItem('romeojuliet-1') })
+    if (localStorage.getItem('bard-buddy')) {
+      let storage = JSON.parse(localStorage.getItem('bard-buddy'))
+      this.setState({ savedPassages: storage })
+    }
+  }
+
+  compileSavedPassages() {
+    return this.state.savedPassages.map(line => {
+      let play = line.play
+      let text = line.text
+      let lineNum = line.lineNum
+      let character = line.character
+      return <PlayLine play={play} text={text} lineNum={lineNum} character={character} />
+    })
   }
 
   render() {
     console.log(this.state)
-    return (
-      <div>
-        <h2>Your saved passages:</h2>
-      </div>
-    )
+    if (!this.state.savedPassages.length) {
+      return (
+        <div>
+          <h2>You have no saved passages!</h2>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Your saved passages:</h2>
+          {this.compileSavedPassages()}
+        </div>
+      )
+    }
   }
 }
